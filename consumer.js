91f7c11c2dc4e2c31996f9fd392295d0ce4f19be
Carlos LM <clagccs@gmail.com>
2018-12-09 22:08:12 -0400

    changed language for messages

--- consumer.js
+++ consumer.js
@@ -2,16 +2,19 @@ const q = "tasks";
 
 async function consumer() {
   try {
+    console.log("[*] Connecting");
     let con = await require("amqplib").connect(
       "amqp://tvobttnq:Ri6S8XroB9gRiIvSesqQMa2KcWI_IOkB@otter.rmq.cloudamqp.com/tvobttnq"
     );
 
     let ch = await con.createChannel();
     let _ok = await ch.assertQueue(q, { durable: false });
+    console.log("[*] Waiting Messages");
+    console.log("------------------------");
     ch.consume(
       q,
       msg => {
-        console.log(msg.content.toString());
+        console.log(`[*] Msg received: ${msg.content.toString()} \n\n`);
       },
       { noAck: true }
     );
